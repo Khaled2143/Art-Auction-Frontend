33661d6aecb4ed7a8c03e6ca809a1909a2b54214
Hamzat Olowu <olowuhamzat@gmail.com>
2023-08-24 18:42:57 -0400

    Revert "navbar done"
    This reverts commit 2cc81b36d78170b6d02f3c45e8d0087d87732f6b.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
       <h1>Code Brigade</h1>
     </div>
   );
