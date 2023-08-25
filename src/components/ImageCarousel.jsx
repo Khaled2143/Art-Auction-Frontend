622066892f7ea1835d1d56f386ac9eeed32fc158
Hamzat Olowu <olowuhamzat@gmail.com>
2023-08-24 21:26:56 -0400

    carousel implemented

---
+++ src/components/ImageCarousel.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const images = [
+  "https://example.com/image1.jpg",
+  "https://example.com/image2.jpg",
+  "https://example.com/image3.jpg",
+  "https://example.com/image4.jpg",
+];
+
+
+function ImageCarousel() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <Slider {...settings}>
+      {images.map((image) => (
+        <div key={image}>
+          <img src={image} alt="carousel slide" />
+        </div>
+      ))}
+    </Slider>
+  );
+}
+
+export default ImageCarousel
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import AnimatedImage from '../components/AnimatedImage'
+import ImageCarousel from '../components/ImageCarousel'
 import "../css/LandingPage.css"
 
 function LandingPage() {
     return (
         <div>
             <AnimatedImage />
+            <ImageCarousel />
         </div>
     )
 }
